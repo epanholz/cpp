f6506f1296913a05a91121b86e1d1dfbc39b4aec
Elisabeth Panholzer <elisabeth@Elisabeths-MacBook.local>
2022-01-24 15:43:21 +0100

    mod0 ex0

--- mod00/ex00/megaphone.cpp
+++ mod00/ex00/megaphone.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int main(void) 
+int main(int argc, char **argv) 
 {
-	std::cout << "Hello World!\n";
+	char c;
+	if(argc>=2) 
+	{
+		for (int i = 1; i <= (argc - 1); i++) 
+		{
+			for(size_t j = 0; j <= strlen(argv[i]); j++) 
+			{
+				c =  argv[i][j];
+				if(c >=97 && c<=122)
+					c = c - 32;
+				std::cout << c;
+			}
+		}
+		std::cout << "\n";
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
     return 0;
 }
