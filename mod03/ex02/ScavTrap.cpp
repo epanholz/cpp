cd160f80d943fbb7be6d2808e4fd888bc06a1231
Elisabeth Panholzer <epanholz@f1r6s4.codam.nl>
2022-06-23 15:46:07 +0200

    added the missing stuffs to the scavtrap files

--- mod03/ex02/ScavTrap.cpp
+++ mod03/ex02/ScavTrap.cpp
@@ -17,6 +17,23 @@ ScavTrap::ScavTrap(std::string name){
 	std::cout << "ScavTrap name constructor called :x" << std::endl;
 }
 
+ScavTrap::ScavTrap(const ScavTrap &old_obj){
+	std::cout << "Copy constructor called :D" << std::endl;
+	this->name = old_obj.name;
+	this->hit_points = old_obj.hit_points;
+	this->energy_points = old_obj.energy_points;
+	this->attack_damage = old_obj.attack_damage;
+}
+
+ScavTrap& ScavTrap::operator=(const ScavTrap &old_obj){
+	std::cout << "Copy assignment operator called ^-^" << std::endl;
+	this->name = old_obj.name;
+	this->hit_points = old_obj.hit_points;
+	this->energy_points = old_obj.energy_points;
+	this->attack_damage = old_obj.attack_damage;
+	return (*this);
+}
+
 ScavTrap::~ScavTrap(){
 	std::cout << "ScavTrap deconstructor called :x" << std::endl;
 }
--- mod03/ex03/ScavTrap.hpp
+++ mod03/ex03/ScavTrap.hpp
@@ -6,6 +6,8 @@ class ScavTrap : virtual public ClapTrap {
 public: /* constructors and deconstructors */
 	ScavTrap();
 	ScavTrap(std::string name);
+	ScavTrap(const ScavTrap &old_obj);
+	ScavTrap& operator=(const ScavTrap &old_obj);
 	~ScavTrap();
 
 public: /* member functions */
